5653a2e11a5bb8aa13d18c02c28ca11125a76f7d
Tatsuya Sato <tatsuya.sato.so@hitachi.com>
2023-05-10 00:11:47 +0000

    chore(integration): Bump k8s-fabric-peer to v0.11.0
    This patch bumps fabric-peer with k8s CC builder to v0.11.0
    based on Fabric v2.5 peer image.
    
    Signed-off-by: Tatsuya Sato <tatsuya.sato.so@hitachi.com>

--- integration/utils/base-step-class.ts
+++ integration/utils/base-step-class.ts
@@ -31,7 +31,7 @@ export default class BaseStepClass {
   protected static FABRIC_TWO_DIGIT_VERSION = process.env.FABRIC_TWO_DIGIT_VERSION ? process.env.FABRIC_TWO_DIGIT_VERSION : '2.5';
 
   protected static FABRIC_VERSION_MAP: { [char: string]: FabricVersion } = {
-    '2.5': { fabric: '2.5.1', fabricCA: '1.5.6', k8sFabricPeer: '0.10.0' },
+    '2.5': { fabric: '2.5.1', fabricCA: '1.5.6', k8sFabricPeer: '0.11.0' },
     '2.2': { fabric: '2.2.11', fabricCA: '1.5.6' },
   }
 
